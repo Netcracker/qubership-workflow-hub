4af1f9f4a2b2097db5ff727adc7cc30bbfa989b7
Pavel Anikin <snwteam5@gmail.com>
2025-03-28 12:13:08 +0400

    fix: Change error handling to warning for missing configuration file in metadata action

--- actions/metadata-action/dist/index.js
+++ actions/metadata-action/dist/index.js
@@ -39926,7 +39926,7 @@ class ConfigLoader {
     console.log(`💡 Try to reading configuration ${configPath}`)
 
     if (!fs.existsSync(configPath)) {
-      core.setFailed(`❗️ File not found: ${configPath}`);
+      core.warning(`❗️ File not found: ${configPath}`);
       return;
     }
 
