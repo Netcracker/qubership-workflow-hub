3b19491af7d94c20f1e836580856a2d0da99d542
borislavr <boris.lavr@gmail.com>
2025-04-10 14:32:23 +0300

    fix: Correct echo statements and ensure PROFILE_ARG is used in Maven commands

--- actions/maven-release/action-script.sh
+++ actions/maven-release/action-script.sh
@@ -59,27 +59,27 @@ function bump_version_and_build() {
     git config --global user.name "qubership-actions[bot]"
     git config --global user.email "qubership-actions[bot]@users.noreply.github.com"
     if [ "${DRY_RUN}" != "false" ]; then
-        echo "::group::Buildin ${MODULE} current version."
+        echo "::group::Building ${MODULE} current version."
         echo "Dry run. Not bumping version."
-        mvn --batch-mode deploy $MVN_ARGS
-        echo "::endgroup::"
+        mvn --batch-mode deploy $MVN_ARGS ${PROFILE_ARG}
         if [ $? -ne 0 ]; then
             echo "Build failed. Exiting."
             echo "❌ Dry-run: build ${MODULE} version ${RELEASE_VERSION} failsed." >> $GITHUB_STEP_SUMMARY
             exit 1
         fi
+        echo "::endgroup::"
         export RELEASE_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
         echo "✔️ Dry-run: Successfully built ${MODULE} version ${RELEASE_VERSION}" >> $GITHUB_STEP_SUMMARY
     else
         echo "::group::Preparing ${MODULE} release."
         mvn --batch-mode versions:use-releases -DgenerateBackupPoms=false
         mvn --batch-mode release:prepare -DautoVersionSubmodules=true -DpushChanges=true -DtagNameFormat="v@{project.version}" ${RELEASE_VERSION_ARG} ${PROFILE_ARG}
-        echo "::endgroup::"
         if [ $? -ne 0 ]; then
             echo "Release preparation failed. Exiting."
             echo "❌ Release: preparation of ${MODULE} version ${RELEASE_VERSION} release failed." >> $GITHUB_STEP_SUMMARY
             exit 1
         fi
+        echo "::endgroup::"
         # scm.tag=v2.0.2
         export RELEASE_VERSION=$(sed -n "s/scm.tag=v//p" release.properties)
         echo "✅ Release: successfully prepared ${MODULE} version ${RELEASE_VERSION} release." >> $GITHUB_STEP_SUMMARY
@@ -91,13 +91,13 @@ function bump_version_and_build() {
         return
     fi
     echo "::group::Releasing ${MODULE} version ${RELEASE_VERSION}"
-    mvn --batch-mode release:perform -DpushChanges=true
-    echo "::endgroup::"
+    mvn --batch-mode release:perform -DpushChanges=true ${PROFILE_ARG}
     if [ $? -ne 0 ]; then
         echo "Release perform failed. Exiting."
         echo "❌ Release: ${MODULE} version ${RELEASE_VERSION} releas failed." >> $GITHUB_STEP_SUMMARY
         exit 1
     fi
+    echo "::endgroup::"
     echo "✅ Release: ${MODULE} version ${RELEASE_VERSION} released successfully." >> $GITHUB_STEP_SUMMARY
 }
 
@@ -110,22 +110,22 @@ function bump_dependencies_versions() {
     fi
     export VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
     echo "::group::Building ${MODULE} version ${VERSION}"
-    mvn --batch-mode deploy -DskipTests=true $MVN_ARGS
-    echo "::endgroup::"
+    mvn --batch-mode deploy -DskipTests=true $MVN_ARGS ${PROFILE_ARG}
     if [ $? -ne 0 ]; then
         echo "Build failed. Exiting."
         echo "❌ Build: ${MODULE} version ${VERSION} failed." >> $GITHUB_STEP_SUMMARY
         exit 1
     fi
+    echo "::endgroup::"
     echo "✅ Build: ${MODULE} version ${VERSION} built successfully." >> $GITHUB_STEP_SUMMARY
     echo "::group::Updating ${MODULE} dependencies versions to next-snapshot"
     mvn --batch-mode versions:use-next-snapshots -DgenerateBackupPoms=false -Dincludes="org.qubership.cloud*:*,org.qubership.core*:*"
-    echo "::endgroup::"
     if [ $? -ne 0 ]; then
         echo "Update dependencies failed. Exiting."
         echo "❌ Update: ${MODULE} dependencies versions to next-snapshot failed." >> $GITHUB_STEP_SUMMARY
         exit 1
     fi
+    echo "::endgroup::"
     echo "::group::Clean and commit pom.xml with next-snapshot version."
     echo "Committing pom.xml with release version."
     mvn --batch-mode clean
