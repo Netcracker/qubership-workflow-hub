7b8bab33ee656330c41a322334bb136eca324869
Pavel Anikin <snwteam5@gmail.com>
2025-04-11 14:37:43 +0400

    Fix ref input handling in metadata action to support fallback for pull requests

--- actions/metadata-action/src/index.js
+++ actions/metadata-action/src/index.js
@@ -84,7 +84,10 @@ function fillTemplate(template, values) {
 async function run() {
   // const def_template = core.getInput("default-template");
 
-  const name = core.getInput('ref') || github.context.ref || github.event.pull_request.head.ref;
+  const name = core.getInput('ref');
+  if (!name) {
+    name = github.context.eventName === 'pull_request' && github.event.pull_request ? github.event.pull_request.head.ref : github.context.ref;
+  }
 
   core.info(`🔹 Ref: ${name}`);
 
