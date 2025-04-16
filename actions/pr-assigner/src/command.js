b78cb4f0ec46c6efeb9693415a8f29ed9d9f4bb9
Pavel Anikin <snwteam5@gmail.com>
2025-04-16 17:56:06 +0400

    refactor: remove stdio inheritance from execSync in getAssigneesCommand and addAssigneesCommand methods

--- actions/pr-assigner/src/command.js
+++ actions/pr-assigner/src/command.js
@@ -5,12 +5,12 @@ class GhCommand {
     }
     getAssigneesCommand(prNumber) {
         let cmd = `gh pr view ${prNumber} --json assignees --jq ".assignees | map(.login) | join(\\" \\" )"`;
-        return execSync(cmd, { stdio: 'inherit' }).toString().trim();
+        return execSync(cmd).toString().trim();
     }
 
     addAssigneesCommand(prNumber, assignees) {
         let cmd = `gh pr edit ${prNumber} ${assignees.map(user => `--add-assignee ${user}`).join(' ')}`;
-        return  execSync(cmd, { stdio: 'inherit' });
+        return  execSync(cmd);
     }
 }
 
