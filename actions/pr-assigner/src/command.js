131620758a22331dbb5c6b3c983f8d180814878f
Pavel Anikin <snwteam5@gmail.com>
2025-04-16 12:01:51 +0400

    fix: remove unnecessary string trimming from execSync return values in command methods

--- actions/pr-assigner/src/command.js
+++ actions/pr-assigner/src/command.js
@@ -5,12 +5,12 @@ class GhCommand {
     }
     async getAssigneesCommand(prNumber) {
         let cmd = `gh pr view ${prNumber} --json assignees --jq ".assignees | map(.login) | join(\\" \\" )"`;
-        return execSync(cmd, { stdio: 'inherit' }).toString().trim();
+        return execSync(cmd, { stdio: 'inherit' });
     }
 
     async addAssigneesCommand(prNumber, assignees) {
         let cmd = `gh pr edit ${prNumber} ${assignees.map(user => `--add-assignee ${user}`).join(' ')}`;
-        return  execSync(cmd, { stdio: 'inherit' }).toString().trim();
+        return  execSync(cmd, { stdio: 'inherit' });
     }
 }
 
