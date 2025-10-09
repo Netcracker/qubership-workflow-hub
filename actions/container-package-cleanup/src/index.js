bec22dcfbd19599026ee0c663d5a8fde3be3cefd
Pavel Anikin <snwteam5@gmail.com>
2025-10-09 13:23:33 +0400

    fix: correct radix parameter in parseInt for thresholdVersions

--- actions/container-package-cleanup/src/index.js
+++ actions/container-package-cleanup/src/index.js
@@ -44,7 +44,7 @@ async function run() {
 
   const now = new Date();
   const thresholdDate = new Date(now.getTime() - thresholdDays * 24 * 60 * 60 * 1000);
-  const thresholdVersions = parseInt(core.getInput('threshold-versions'), 0);
+  const thresholdVersions = parseInt(core.getInput('threshold-versions'), 10);
 
   // core.info(`Configuration Path: ${configurationPath}`);
   core.info(`Threshold Days: ${thresholdDays}`);
